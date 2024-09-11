51eb8756682c768035b18c67acb0861acdfa9d0d
Dmitry Dudkin <leman111000@gmail.com>
2024-09-11 16:03:25 +0300

    update

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ def main():
         num2 = int(c)
         if num1 > 10 or num2 > 10:
             print("Одно из чисел больше 10")
+        elif num1 < 1 or num2 < 1:
+            print("Одно из чисел меньше 1")
         else:
             result = eval(f"{a} {b} {c}")
-            print(int(result))
+            new_result = int(result)
+            print(str(new_result))
     except Exception as e:
         print(f"Произошла ошибка: {e}")
 
